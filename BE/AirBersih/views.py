7737fa0555d67b333266be74c11acefb87a898b8
Abi Yasid Al'Majid <142866105+AbiYasidAlMajid@users.noreply.github.com>
2024-12-12 16:11:04 +0700

    Update views.py

--- BE/AirBersih/views.py
+++ BE/AirBersih/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status, generics
+from rest_framework import viewsets, status, generics, views, permissions
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import AirBersihAdmin, Lokasi, GolonganPelanggan, Pengguna, Petugas, SumberAir, Pengaduan, Tagihan, Pembayaran
 from .serializers import LoginSerializer, LogoutSerializer, RegisterSerializer, AirBersihAdminSerializer, LokasiSerializer, GolonganPelangganSerializer, PenggunaSerializer, PetugasSerializer, SumberAirSerializer, PengaduanSerializer, TagihanSerializer, PembayaranSerializer
